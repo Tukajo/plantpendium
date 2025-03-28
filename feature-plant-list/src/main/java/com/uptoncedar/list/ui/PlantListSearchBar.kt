0fdcaa0a1330bcf636bf888ff880c3016cd16870
Tukajo <tjfrisch@gmail.com>
2025-03-28 15:10:09 -0500

    change the keyboard action to show a "search" circle for submission via keyboard.

--- feature-plant-list/src/main/java/com/uptoncedar/list/ui/PlantListSearchBar.kt
+++ feature-plant-list/src/main/java/com/uptoncedar/list/ui/PlantListSearchBar.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Search
@@ -19,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -37,7 +40,11 @@ fun PlantListSearchBar(
             onValueChange = { searchText = it },
             leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "Search") },
             placeholder = { Text("Search Plants") },
-            modifier = Modifier.weight(1f) // Occupy most of the width
+            modifier = Modifier.weight(1f),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(
+                onSearch = { onSearchSubmit(searchText) }
+            )
         )
         Spacer(modifier = Modifier.width(8.dp))
         Button(onClick = { onSearchSubmit(searchText) }) {
