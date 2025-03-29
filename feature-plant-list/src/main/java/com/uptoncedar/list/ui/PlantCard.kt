936b5087f2f19411eab0b9c5b959885a8bd7625c
Tukajo <tjfrisch@gmail.com>
2025-03-29 17:16:28 -0500

    Change plantcard to respect the color scheme of the user

--- feature-plant-list/src/main/java/com/uptoncedar/list/ui/PlantCard.kt
+++ feature-plant-list/src/main/java/com/uptoncedar/list/ui/PlantCard.kt
@@ -1,16 +1,15 @@
 package com.uptoncedar.list.ui
 
+
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -23,49 +22,61 @@ fun PlantCard(
     plantListEntry: PlantListEntry,
     onNavigateToDetails: (plantId: String) -> Unit,
 ) {
-    Column(
+    Card(
         modifier = Modifier
             .padding(8.dp)
-            .border(1.dp, Color.LightGray, RoundedCornerShape(8.dp))
-            .clickable { onNavigateToDetails(plantListEntry.id) }
+            .clickable { onNavigateToDetails(plantListEntry.id) },
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            modifier = Modifier.padding(16.dp)
         ) {
-            SubcomposeAsyncImage(
-                modifier = Modifier.size(100.dp),
-                model = plantListEntry.image_url,
-                contentDescription = plantListEntry.scientific_name,
-                loading = {
-                    CircularProgressIndicator()
-                },
-                error = {
-                    Image(
-                        painter = painterResource(id = com.uptoncedar.list.R.drawable.image_placeholder_icon),
-                        contentDescription = "Image not available",
-                        modifier = Modifier.size(100.dp)
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Column(
-                modifier = Modifier.padding(8.dp)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
             ) {
-                plantListEntry.common_name?.let {
-                    Text(
-                        text = it,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 18.sp
-                    )
-                }
-                Spacer(modifier = Modifier.height(4.dp))
-                plantListEntry.scientific_name?.let {
-                    Text(
-                        text = it,
-                        fontSize = 14.sp,
-                        color = Color.Gray
-                    )
+                SubcomposeAsyncImage(
+                    modifier = Modifier.size(100.dp),
+                    model = plantListEntry.image_url,
+                    contentDescription = plantListEntry.scientific_name,
+                    loading = {
+                        CircularProgressIndicator()
+                    },
+                    error = {
+                        Image(
+                            painter = painterResource(id = com.uptoncedar.list.R.drawable.image_placeholder_icon),
+                            contentDescription = "Image not available",
+                            modifier = Modifier.size(100.dp),
+                            colorFilter = ColorFilter.tint(LocalContentColor.current)
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                VerticalDivider(
+                    color = Color.LightGray,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(1.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    plantListEntry.common_name?.let {
+                        Text(
+                            text = it,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    plantListEntry.scientific_name?.let {
+                        Text(
+                            text = it,
+                            fontSize = 14.sp,
+                            color = Color.Gray
+                        )
+                    }
                 }
             }
         }
