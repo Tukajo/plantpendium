8cf09599457d299b36bf3524c08a01da24c394f0
Tukajo <tjfrisch@gmail.com>
2025-03-29 20:07:17 -0500

    lightweight caching on usecases to avoid repeated API calls during a single app use lifespan

--- feature-plant-details/src/main/java/com/uptoncedar/plant/details/domain/GetPlantByIdUseCase.kt
+++ feature-plant-details/src/main/java/com/uptoncedar/plant/details/domain/GetPlantByIdUseCase.kt
@@ -3,20 +3,31 @@ package com.uptoncedar.plant.details.domain
 import com.uptoncedar.common.model.PlantDetails
 import com.uptoncedar.networking.api.FloraCodexApi
 import javax.inject.Inject
+import javax.inject.Singleton
 
 interface GetPlantByIdUseCase {
     suspend operator fun invoke(id: String): PlantDetails
 }
 
+@Singleton
 class GetPlantByIdUseCaseImpl @Inject constructor(
     private val floraCodexApi: FloraCodexApi
 ) : GetPlantByIdUseCase {
+
+    private val cache = mutableMapOf<String, PlantDetails>()
+
     override suspend operator fun invoke(id: String): PlantDetails {
-        val res = floraCodexApi.getPlantById(id = id)
-        return when (res.isSuccessful) {
-            true -> res.body() ?: throw Exception("Plant details not found")
-            else -> {
-                throw Exception("Error, unable to retrieve plant details")
+        return cache[id] ?: run {
+            val res = floraCodexApi.getPlantById(id = id)
+            when (res.isSuccessful) {
+                true -> {
+                    val plantDetails = res.body() ?: throw Exception("Plant details not found")
+                    cache[id] = plantDetails
+                    plantDetails
+                }
+                else -> {
+                    throw Exception("Error, unable to retrieve plant details")
+                }
             }
         }
     }
--- feature-plant-list/src/main/java/com/uptoncedar/list/domain/GetPlantsByQueryUseCase.kt
+++ feature-plant-list/src/main/java/com/uptoncedar/list/domain/GetPlantsByQueryUseCase.kt
@@ -3,20 +3,31 @@ package com.uptoncedar.list.domain
 import com.uptoncedar.common.model.PlantListEntry
 import com.uptoncedar.networking.api.FloraCodexApi
 import javax.inject.Inject
+import javax.inject.Singleton
 
 interface GetPlantsByQueryUseCase {
     suspend operator fun invoke(query: String): List<PlantListEntry>
 }
 
+@Singleton
 class GetPlantsByQueryUseCaseImpl @Inject constructor(
     private val floraCodexApi: FloraCodexApi
 ) : GetPlantsByQueryUseCase {
+
+    private val cache = mutableMapOf<String, List<PlantListEntry>>()
+
     override suspend operator fun invoke(query: String): List<PlantListEntry> {
-        val res = floraCodexApi.getPlants(query = query)
-        return when (res.isSuccessful) {
-            true -> res.body()?.data ?: emptyList()
-            else -> {
-                throw Exception("Error, unable to retrieve result")
+        return cache[query] ?: run {
+            val res = floraCodexApi.getPlants(query = query)
+            when (res.isSuccessful) {
+                true -> {
+                    val plantList = res.body()?.data ?: emptyList()
+                    cache[query] = plantList
+                    plantList
+                }
+                else -> {
+                    throw Exception("Error, unable to retrieve result")
+                }
             }
         }
     }
