bcea9b1cc5c1c4ec17290f162f9b778218cca706
Tukajo <tjfrisch@gmail.com>
2025-03-29 19:28:14 -0500

    swap to when statement for PlantDetailsScreen.kt

--- feature-plant-details/src/main/java/com/uptoncedar/plant/details/ui/PlantDetailsScreen.kt
+++ feature-plant-details/src/main/java/com/uptoncedar/plant/details/ui/PlantDetailsScreen.kt
@@ -31,103 +31,105 @@ fun PlantDetailsScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        if (isLoading) {
-            CircularProgressIndicator()
-        } else if (errorMessage != null) {
-            Text(
-                text = "Error: $errorMessage",
-                color = MaterialTheme.colorScheme.error,
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Button(onClick = { viewModel.clearError() }) {
-                Text(stringResource(android.R.string.ok))
-            }
-        } else if (plantDetails != null) {
-            plantDetails?.main_species?.image_url?.let { imageUrl ->
-                ElevatedCard(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 2.dp),
-                    elevation = CardDefaults.cardElevation(
-                        defaultElevation = 8.dp
-                    )
-                ) {
-                    FillingImage(
-                        imageUrl = imageUrl,
-                        imageDescription = plantDetails?.common_name,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(200.dp)
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Card(modifier = Modifier.fillMaxWidth()) {
-                Column(modifier = Modifier.padding(16.dp)) {
+        when (isLoading) {
+            true -> CircularProgressIndicator()
+            else -> {
+                if (errorMessage != null) {
                     Text(
-                        text = stringResource(R.string.common_section_title),
-                        style = MaterialTheme.typography.headlineSmall
+                        text = "Error: $errorMessage",
+                        color = MaterialTheme.colorScheme.error,
+                        style = MaterialTheme.typography.bodyMedium
                     )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    plantDetails?.common_name?.let {
-                        Row {
-                            Text(
-                                text = stringResource(R.string.common_name_field_label),
-                                style = MaterialTheme.typography.titleMedium
+                    Button(onClick = { viewModel.clearError() }) {
+                        Text(stringResource(android.R.string.ok))
+                    }
+                } else if (plantDetails != null) {
+                    plantDetails?.main_species?.image_url?.let { imageUrl ->
+                        ElevatedCard(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 2.dp),
+                            elevation = CardDefaults.cardElevation(
+                                defaultElevation = 8.dp
                             )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(text = it, style = MaterialTheme.typography.bodyLarge)
+                        ) {
+                            FillingImage(
+                                imageUrl = imageUrl,
+                                imageDescription = plantDetails?.common_name,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(200.dp)
+                            )
                         }
                     }
-                }
-            }
+                    Spacer(modifier = Modifier.height(16.dp))
 
-            Card(modifier = Modifier.fillMaxWidth()) {
-                Column(modifier = Modifier.padding(16.dp)) {
+                    Card(modifier = Modifier.fillMaxWidth()) {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(
+                                text = stringResource(R.string.common_section_title),
+                                style = MaterialTheme.typography.headlineSmall
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            plantDetails?.common_name?.let {
+                                Row {
+                                    Text(
+                                        text = stringResource(R.string.common_name_field_label),
+                                        style = MaterialTheme.typography.titleMedium
+                                    )
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text(text = it, style = MaterialTheme.typography.bodyLarge)
+                                }
+                            }
+                        }
+                    }
+                    Card(modifier = Modifier.fillMaxWidth()) {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(
+                                text = stringResource(R.string.scientific_classification_section_title),
+                                style = MaterialTheme.typography.headlineSmall
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            plantDetails?.scientific_name?.let {
+                                Row {
+                                    Text(
+                                        text = stringResource(R.string.scientific_name_field_label),
+                                        style = MaterialTheme.typography.titleMedium
+                                    )
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text(text = it, style = MaterialTheme.typography.bodyLarge)
+                                }
+                            }
+                            plantDetails?.main_species?.family?.let {
+                                Row {
+                                    Text(
+                                        text = stringResource(R.string.scientific_family_field_label),
+                                        style = MaterialTheme.typography.titleMedium
+                                    )
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text(text = it, style = MaterialTheme.typography.bodyLarge)
+                                }
+                            }
+                            plantDetails?.main_species?.genus?.let {
+                                Row {
+                                    Text(
+                                        text = stringResource(R.string.scientific_genus_field_label),
+                                        style = MaterialTheme.typography.titleMedium
+                                    )
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Text(text = it, style = MaterialTheme.typography.bodyLarge)
+                                }
+                            }
+                        }
+
+                    }
+                } else {
                     Text(
-                        text = stringResource(R.string.scientific_classification_section_title),
-                        style = MaterialTheme.typography.headlineSmall
+                        stringResource(R.string.no_plant_details_found_warning),
+                        style = MaterialTheme.typography.bodyMedium
                     )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    plantDetails?.scientific_name?.let {
-                        Row {
-                            Text(
-                                text = stringResource(R.string.scientific_name_field_label),
-                                style = MaterialTheme.typography.titleMedium
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(text = it, style = MaterialTheme.typography.bodyLarge)
-                        }
-                    }
-                    plantDetails?.main_species?.family?.let {
-                        Row {
-                            Text(
-                                text = stringResource(R.string.scientific_family_field_label),
-                                style = MaterialTheme.typography.titleMedium
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(text = it, style = MaterialTheme.typography.bodyLarge)
-                        }
-                    }
-                    plantDetails?.main_species?.genus?.let {
-                        Row {
-                            Text(
-                                text = stringResource(R.string.scientific_genus_field_label),
-                                style = MaterialTheme.typography.titleMedium
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(text = it, style = MaterialTheme.typography.bodyLarge)
-                        }
-                    }
                 }
             }
-
-        } else {
-            Text(
-                stringResource(R.string.no_plant_details_found_warning),
-                style = MaterialTheme.typography.bodyMedium
-            )
         }
     }
 }
